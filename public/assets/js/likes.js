a08448175a5f7cce3cda521bbf55a3b3d89f6227
Mauricio Andres Muñoz Sanchez <mauricio.andres.munoz@correounivalle.edu.co>
2025-05-08 21:34:25 -0500

    function like js

--- public/assets/js/likes.js
+++ public/assets/js/likes.js
@@ -1,3 +1,40 @@
+function toggleLike(postId) {
+            const isLiked = this.classList.contains('btn-success'); // Verificar si ya está "liked"
+            button.disabled = true;
+            //Cambiar clases al boton, y para eso necesito traer el boton, tengo que cambiar todos los this
+            
+            // Enviar solicitud al servidor para dar o quitar like
+            fetch(`/post/${postId}/like`, {
+                method: 'POST',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify({ like: !isLiked }), // Enviar true si no está "liked", false si ya lo está
+            })
+                .then(response => response.json())
+                .then(data => {
+                    if (data.success) {
+                        // Alternar el estado del botón clicado
+                        if (isLiked) {
+                            this.classList.remove('btn-success');
+                            this.classList.add('btn-primary');
+                            this.innerHTML = '<i class="bi bi-hand-thumbs-up"></i>';
+                        } else {
+                            this.classList.remove('btn-primary');
+                            this.classList.add('btn-success');
+                            this.innerHTML = '<i class="bi bi-hand-thumbs-up-fill"></i>';
+                        }
+                    } else {
+                        alert('Error al actualizar el like');
+                    }
+                })
+                .finally(() => {
+                    button.disabled = false; // Reactiva el botón después de la petición
+                })
+                .catch(error => console.error('Error:', error));
+}
+
+
+
+
 document.addEventListener('DOMContentLoaded', function () {
     const likeButtons = document.querySelectorAll('.like-btn');
     validateLikes();
