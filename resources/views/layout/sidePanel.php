23e8af1fdb03889a1ce3dc0e5f7d3dcd227af58c
Mauricio Andres Muñoz Sanchez <mauricio.andres.munoz@correounivalle.edu.co>
2025-05-22 15:13:26 -0500

    add styles CSS to sidePanel

--- resources/views/layout/sidePanel.php
+++ resources/views/layout/sidePanel.php
@@ -1,5 +1,7 @@
 <!-- Side Panel -->
- <link rel="stylesheet" href="/assets/css/style.css"> 
+<link rel="stylesheet" href="/assets/css/style.css"> 
+<link rel="stylesheet" href="/assets/css/side-panel.css">
+
 <div class="offcanvas offcanvas-start sidebar-custom" tabindex="-1" id="sidebarMenu" aria-labelledby="sidebarMenuLabel">
   <div class="offcanvas-header">
     <div style="display: flex; flex-direction: column; align-items: center; justify-content: center; width: 100%;">
@@ -10,29 +12,26 @@
     </div>
     <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Cerrar"></button>
   </div>
-  <div class="offcanvas-body p-0">
+  <div class="offcanvas-body  d-flex flex-column align-items-center">
     <ul class="nav flex-column">
       <li class="nav-item border-0">
-        <a class="nav-link active" aria-current="page" href="/"><i class="bi bi-house-fill"></i></a>
+        <a class="nav-link  d-flex  align-items-center icon-lg" aria-current="page" href="/"><i class="bi bi-house-fill"></i></a>
       </li>
       <li class="nav-item border-0">
-        <a class="nav-link" href="#" data-bs-toggle="modal" data-bs-target="#chatModal"><i class="bi bi-wechat"></i></a>
+        <a class="nav-link d-flex  align-items-center icon-lg" href="#" data-bs-toggle="modal" data-bs-target="#chatModal"><i class="bi bi-wechat"></i></a>
       </li>
       <li class="nav-item border-0">
-        <a class="nav-link" href="#" data-bs-toggle="modal" data-bs-target="#createPostModal"><i class="bi bi-upload"></i></a>
+        <a class="nav-link d-flex  align-items-center icon-lg" href="#" data-bs-toggle="modal" data-bs-target="#createPostModal"><i class="bi bi-upload"></i></a>
       </li>
        <li class="nav-item border-0">
-        <a class="nav-link" href="#" data-bs-toggle="modal" data-bs-target="#interestsModal"><i class="bi bi-person-plus-fill"></i></a>
+        <a class="nav-link d-flex  align-items-center icon-lg" href="#" data-bs-toggle="modal" data-bs-target="#interestsModal"><i class="bi bi-person-plus-fill"></i></a>
       </li>
-      
-
       <li class="nav-item">
         <!-- Botón de logout -->
-        <a href="/logout" class="btn btn-outline-danger btn-sm ms-3 mt-5 rounded-pill ">Cerrar sesión</a>
+        <a href="/logout" class="btn btn-outline-danger btn-sm  mt-5 rounded-pill ">Cerrar sesión</a>
       </li>
     </ul>
   </div>
 </div>
 
 
-<script src="/assets/js/search.js"></script>
