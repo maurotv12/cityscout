e5b480b93c333451abc33990f3fdcdc4c1068f2d
Mauricio Andres Muñoz Sanchez <mauricio.andres.munoz@correounivalle.edu.co>
2025-06-03 06:32:03 -0500

    css login and register

--- resources/views/auth/login.php
+++ resources/views/auth/login.php
@@ -5,63 +5,51 @@ $error = $_SESSION['error'] ?? null;
 unset($_SESSION['error']);
 ?>
 
-<form action="/login" method="POST">
-
-    <div class="container h-100">
-        <div class="row d-flex justify-content-center  h-100">
-            <div class="col-xl-10">
-                <div class="card rounded-3 text-black">
-                    <div class="row g-0">
-                        <div class="col-lg-6">
-                            <div class="card-body p-md-5 mx-md-4">
-
-                                <div class="text-center">
-                                    <img src="/assets/images/logo.png"
-                                        style="width: 185px;" alt="logo">
-                                    <h3 class="mt-1 mb-5 pb-1">FOCUZ</h3>
+<div class="container h-100">
+    <div class="row d-flex justify-content-center align-items-center h-100">
+        <div class="col-xl-10">
+            <div class="card rounded-3 text-black login-card" style="min-height: 80vh;">
+                <div class="row g-0">
+                    <div class="col-lg-6 d-flex align-items-center">
+                        <div class="card-body p-md-4 mx-md-3 w-100">
+                            <div class="text-center">
+                                <img src="/assets/images/logo.png"
+                                    class="login-logo"
+                                    alt="logo">
+                                <h3 class="mt-1 mb-4 pb-1">FOCUZ</h3>
+                            </div>
+                            <form action="/login" method="POST" class="w-100">
+                                <p class="mb-3">Por favor ingresa a tu cuenta</p>
+                                <div class="form-floating mb-3">
+                                    <input type="text" id="inputUser" class="form-control login-input" required name="userName" placeholder="Nombre de Usuario" />
+                                    <label class="form-label" for="floatingInput">Nombre de Usuario</label>
                                 </div>
-
-                                <form>
-                                    <p>Por favor ingresa a tu cuenta</p>
-
-                                    <div data-mdb-input-init class="form-outline ">
-                                        <input type="text" id="inputUser" class="form-control" required name="userName" />
-                                        <label class="form-label" for="inputUser">Nombre de Usuario</label>
-                                    </div>
-
-                                    <div data-mdb-input-init class="form-outline mb-4">
-                                        <input type="password" id="inputPassword" class="form-control" required name="password" />
-                                        <label class="form-label" for="inputPassword">Contraseña</label>
-                                    </div>
-
-                                    <div class="text-center pt-1 mb-5 pb-1">
-                                        <button type="submit" data-mdb-button-init data-mdb-ripple-init class="btn btn-primary btn-block fa-lg gradient-custom-2 mb-3" type="button">Ingresar</button>
-                                        <a class="text-muted" href="#!">Olvidaste tu contraseña?</a>
-                                    </div>
-
-                                    <div class="d-flex align-items-center justify-content-center ">
-                                        <p class="mb-0 me-2">No tienes una cuenta?</p>
-                                        <a href="register" class="btn btn-primary btn-block gradient-custom-2">Regístrate</a>
-                                    </div>
-
-                                </form>
-
-                            </div>
+                                <div class="form-floating mb-3">
+                                    <input type="password" id="inputPassword" class="form-control login-input" required name="password" placeholder="Contraseña"/>
+                                    <label class="form-label" for="inputPassword">Contraseña</label>
+                                </div>
+                                <div class="text-center pt-1 mb-4 pb-1">
+                                    <button type="submit" class="btn btn-primary btn-block fa-lg gradient-custom-2 mb-2 w-100">Ingresar</button>
+                                    <a class="text-muted" href="#!">Olvidaste tu contraseña?</a>
+                                </div>
+                                <div class="d-flex align-items-center justify-content-center">
+                                    <p class="mb-0 me-2">No tienes una cuenta?</p>
+                                    <a href="register" class="btn btn-primary btn-block gradient-custom-2 ms-2">Regístrate</a>
+                                </div>
+                            </form>
                         </div>
-                        <div class="col-lg-6 d-flex align-items-center gradient-custom-2">
-                            <div class="text-white px-3 py-4 p-md-5 mx-md-4">
-                                <h4 class="mb-4">¡Ingresa y comienza a crear momentos inolvidables!</h4>
-                                <p class="lg mb-0">Comparte tu mundo en imágenes, conecta con quienes comparten tu visión y deja que cada publicación revele tu esencia. Bienvenido al lugar donde tus recuerdos cobran vida y las historias se convierten en inspiración...</p>
-                            </div>
+                    </div>
+                    <div class="col-lg-6 d-flex align-items-center gradient-custom-2">
+                        <div class="text-white px-3 py-4 p-md-5 mx-md-4">
+                            <h4 class="mb-4">¡Ingresa y comienza a crear momentos inolvidables!</h4>
+                            <p class="lg mb-0">Comparte tu mundo en imágenes, conecta con quienes comparten tu visión y deja que cada publicación revele tu esencia. Bienvenido al lugar donde tus recuerdos cobran vida y las historias se convierten en inspiración...</p>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
     </div>
-
-
-</form>
+</div>
 
 <?php if ($error): ?>
     <div class="alert alert-danger">
--- resources/views/auth/register.php
+++ resources/views/auth/register.php
@@ -7,9 +7,9 @@ unset($_SESSION['error']);
 <form method="POST" action="/register" enctype="multipart/form-data">
 
   <div class="container h-100">
-    <div class="row d-flex justify-content-center  h-100">
+    <div class="row d-flex justify-content-center align-items-center h-100">
       <div class="col-xl-10">
-        <div class="card rounded-3 text-black">
+        <div class="card rounded-3 text-black register-card" style="min-height: 80vh;">
           <div class="row g-0">
             <div class="col-lg-6 d-flex align-items-center gradient-custom-2">
               <div class="text-white px-3 py-4 p-md-5 mx-md-4">
@@ -17,49 +17,45 @@ unset($_SESSION['error']);
                 <p class="lg mb-0">Comparte tu mundo en imágenes, conecta con quienes comparten tu visión y deja que cada publicación revele tu esencia. Bienvenido al lugar donde tus recuerdos cobran vida y las historias se convierten en inspiración...</p>
               </div>
             </div>
-              <div class="col-lg-6">
-                <div class="card-body p-md-5 mx-md-4">
-
-                  <div class="text-center">
-                    <img src="/assets/images/logo.png"
-                      style="width: 185px;" alt="logo">
-                    <h3 class="mt-1 mb-5 pb-1">FOCUZ</h3>
-                  </div>
-                  
-                  <div class="col-md-12">
-                    <label class="form-label" for="fullname">Nombre completo *</label>
-                    <input type="text" id="fullname" class="form-control" required name="fullname" />
-                  </div>
-
-                  <div class="col-md-12">
-                    <label class="form-label" for="username">Nombre de usuario *</label>
-                    <input type="text" id="username" class="form-control" required name="username" />
-                    <small class="form-text text-danger" style="display:none;"></small> <!-- Mensaje de error -->
-                  </div>
-
-                  <div class="col-md-12">
-                    <label class="form-label" for="email">Correo *</label>
-                    <input type="email" id="email" class="form-control" required name="email" />
-                    <small class="form-text text-danger" style="display:none;"></small> <!-- Mensaje de error -->
-                  </div>
-
-                  <div class="col-md-12">
-                    <label class="form-label" for="password">Contraseña *</label>
-                    <input type="password" id="password" class="form-control" required name="password" pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}"/>
-                    <p><small class="mt-1 mb-5 pb-1">La contraseña debe tener al menos 8 caracteres, una letra mayúscula, una letra minúscula y un número.</small></p>
-                  </div>
-
-                  <div class="col-md-12">
-                    <label class="form-label" for="birth">Fecha de nacimiento *</label>
-                    <input type="date" id="birth_date" class="form-control" required name="birth_date" min="<?php echo date('Y-m-d', strtotime('-100 years')); ?>" max="<?php echo date('Y-m-d', strtotime('-18 years')); ?>" />
-                  </div>
-
-                  <div class="d-flex align-items-center justify-content-center ">
-                    <p class="mb-0 me-2">¡Estas a solo un paso de crear tu cuenta!</p>
-                    <button type="submit" id="register-button" class="btn btn-primary btn-block gradient-custom-2">Registrarme</button>
-                  </div>
+            <div class="col-lg-6 d-flex align-items-center">
+              <div class="card-body p-md-4 mx-md-3 w-100">
+                <div class="text-center">
+                  <img src="/assets/images/logo.png"
+                    class="login-logo"
+                    alt="logo">
+                  <h3 class="mt-1 mb-4 pb-1">FOCUZ</h3>
                 </div>
+                <form method="POST" action="/register" enctype="multipart/form-data" class="w-100">
+                  <div class="form-floating mb-3">
+                    <input type="text" id="fullname" class="form-control register-input" required name="fullname" placeholder="Nombre completo"/>
+                    <label class="form-label" for="floatingInput">Nombre completo *</label>
+                  </div>
+                  <div class="form-floating mb-3">
+                    <input type="text" id="username" class="form-control register-input" required name="username" placeholder="Nombre de usuario"/>
+                    <label class="form-label" for="floatingInput">Nombre de usuario *</label>
+                    <small class="form-text text-danger" style="display:none;"></small>
+                  </div>
+                  <div class="form-floating mb-3">
+                    <input type="email" id="email" class="form-control register-input" required name="email" placeholder="Correo" />
+                    <label class="form-label" for="email">Correo *</label>
+                    <small class="form-text text-danger" style="display:none;"></small>
+                  </div>
+                  <div class="form-floating mb-3">
+                    <input type="password" id="password" class="form-control register-input" required name="password" pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}" placeholder="Contraseña"/>
+                    <label class="form-label" for="password">Contraseña *</label>
+                    <p><small class="mt-1 mb-2 pb-1">La contraseña debe tener al menos 8 caracteres, una letra mayúscula, una letra minúscula y un número.</small></p>
+                  </div>
+                  <div class="form-floating mb-3">
+                    <input type="date" id="birth_date" class="form-control register-input" required name="birth_date" min="<?php echo date('Y-m-d', strtotime('-100 years')); ?>" max="<?php echo date('Y-m-d', strtotime('-18 years')); ?>" />
+                    <label class="form-label" for="birth">Fecha de nacimiento *</label>
+                  </div>
+                  <div class="d-flex align-items-center justify-content-center">
+                    <p class="mb-0 me-2">¡Estas a solo un paso de crear tu cuenta!</p>
+                    <button type="submit" id="register-button" class="btn btn-primary btn-block gradient-custom-2 ms-2">Registrarme</button>
+                  </div>
+                </form>
               </div>
+            </div>
           </div>
         </div>
       </div>
