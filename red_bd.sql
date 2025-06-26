72517763307ab43e8e9b2701d0c9994e58bc44b4
Mauricio Andres Muñoz Sanchez <mauricio.andres.munoz@correounivalle.edu.co>
2025-06-26 14:26:27 -0500

    bd

--- red_bd.sql
+++ red_bd.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 25-06-2025 a las 19:39:06
+-- Tiempo de generación: 26-06-2025 a las 21:25:10
 -- Versión del servidor: 10.4.32-MariaDB
 -- Versión de PHP: 8.2.12
 
@@ -40,25 +40,14 @@ CREATE TABLE `comments` (
 --
 
 INSERT INTO `comments` (`id`, `user_id`, `post_id`, `comment`, `created_at`) VALUES
-(3, 1, 14, 'hola', '2025-05-01 20:11:32'),
-(4, 1, 21, 'El corte de la falda está muy bueno', '2025-05-01 20:11:51'),
-(5, 1, 21, 'hola', '2025-05-01 20:11:51'),
-(6, 1, 15, 'hola', '2025-05-01 20:12:03'),
-(8, 1, 21, 'holios', '2025-05-01 20:18:29'),
-(9, 1, 21, 'holios', '2025-05-01 20:18:29'),
 (10, 1, 22, 'Buen video', '2025-05-01 20:25:20'),
-(12, 1, 16, 'hol', '2025-05-01 20:29:11'),
-(13, 1, 17, 'holiss', '2025-05-01 20:32:23'),
 (15, 1, 22, 'yyy', '2025-05-01 21:17:21'),
-(17, 7, 14, 'como vas con ese folleto con ta buena información valiosaa', '2025-05-01 22:10:44'),
 (19, 7, 22, 'video', '2025-05-05 04:19:20'),
-(21, 1, 15, 'asdasd', '2025-05-05 22:50:30'),
-(22, 1, 15, 'Los objetivos son muy importantes', '2025-05-06 22:50:50'),
-(23, 1, 15, 'si', '2025-05-06 22:51:41'),
 (25, 1, 24, 'yeyeyteytr', '2025-05-13 18:54:52'),
-(26, 1, 14, 'df', '2025-05-28 22:51:11'),
 (27, 1, 27, 'hi', '2025-06-09 02:54:02'),
-(28, 1, 26, 'hi', '2025-06-09 02:54:22');
+(28, 1, 26, 'hi', '2025-06-09 02:54:22'),
+(31, 1, 39, 'Hombre generaod por IA', '2025-06-26 18:47:43'),
+(32, 1, 39, 'Imagen fondo desenfocado', '2025-06-26 18:48:09');
 
 -- --------------------------------------------------------
 
@@ -79,13 +68,13 @@ CREATE TABLE `followers` (
 
 INSERT INTO `followers` (`id`, `user_follower_id`, `user_followed_id`, `created_at`) VALUES
 (19, 7, 4, '2025-05-07 15:32:16'),
-(26, 1, 12, '2025-05-20 01:12:37'),
 (31, 7, 1, '2025-05-22 00:16:29'),
 (42, 7, 3, '2025-05-22 01:05:54'),
 (43, 7, 9, '2025-05-22 01:06:29'),
 (50, 2, 1, '2025-06-18 18:26:50'),
-(59, 1, 7, '2025-06-25 16:10:11'),
-(60, 1, 9, '2025-06-25 19:07:01');
+(63, 1, 8, '2025-06-26 19:02:32'),
+(64, 1, 7, '2025-06-26 19:05:56'),
+(65, 1, 12, '2025-06-26 19:06:03');
 
 -- --------------------------------------------------------
 
@@ -132,21 +121,21 @@ CREATE TABLE `likes` (
 
 INSERT INTO `likes` (`id`, `user_id`, `post_id`, `created_at`) VALUES
 (109, 1, 25, '2025-05-10 18:10:05'),
-(118, 7, 15, '2025-05-10 19:12:23'),
-(119, 7, 14, '2025-05-10 19:12:24'),
 (120, 7, 27, '2025-05-10 19:12:57'),
 (121, 7, 26, '2025-05-10 19:12:58'),
 (122, 7, 25, '2025-05-10 19:12:59'),
 (124, 1, 27, '2025-05-10 20:20:16'),
 (130, 1, 22, '2025-05-13 18:43:24'),
 (131, 1, 24, '2025-05-13 18:43:25'),
-(132, 1, 21, '2025-05-13 18:43:26'),
-(133, 1, 17, '2025-05-28 22:29:08'),
-(139, 1, 30, '2025-06-09 03:39:11'),
 (141, 1, 32, '2025-06-09 03:39:13'),
-(142, 1, 31, '2025-06-17 04:28:44'),
-(149, 1, 14, '2025-06-25 16:09:45'),
-(150, 1, 15, '2025-06-25 17:36:41');
+(153, 7, 33, '2025-06-26 17:32:24'),
+(154, 7, 35, '2025-06-26 17:33:13'),
+(155, 7, 34, '2025-06-26 17:33:14'),
+(156, 7, 36, '2025-06-26 17:33:15'),
+(157, 1, 38, '2025-06-26 18:46:54'),
+(158, 1, 37, '2025-06-26 18:46:54'),
+(159, 8, 37, '2025-06-26 18:47:00'),
+(160, 8, 38, '2025-06-26 18:47:01');
 
 -- --------------------------------------------------------
 
@@ -187,7 +176,8 @@ INSERT INTO `messages` (`id`, `sender_id`, `receiver_id`, `message`, `is_read`, 
 (17, 1, 7, 'holaaaa', 1, '2025-05-05 15:40:02'),
 (18, 7, 1, 'camilo', 1, '2025-05-05 15:40:10'),
 (19, 2, 1, 'Hola Mauricio como estas', 0, '2025-06-18 18:26:59'),
-(20, 1, 9, 'Hola', 0, '2025-06-24 18:08:54');
+(20, 1, 9, 'Hola', 0, '2025-06-24 18:08:54'),
+(21, 1, 7, 'Que tal el día?', 0, '2025-06-26 16:30:12');
 
 -- --------------------------------------------------------
 
@@ -296,18 +286,18 @@ INSERT INTO `notifications` (`id`, `user_id`, `sender_id`, `type`, `reference_id
 (83, 12, 1, 'follower', NULL, 'Te ha seguido.', 0, '2025-05-20 01:12:37'),
 (84, 9, 1, 'follower', NULL, 'Te ha seguido.', 0, '2025-05-21 23:39:45'),
 (85, 9, 1, 'follower', NULL, 'Te ha seguido.', 0, '2025-05-22 00:14:30'),
-(86, 8, 1, 'follower', NULL, 'Te ha seguido.', 0, '2025-05-22 00:14:32'),
-(87, 8, 1, 'follower', NULL, 'Te ha seguido.', 0, '2025-05-22 00:14:33'),
+(86, 8, 1, 'follower', NULL, 'Te ha seguido.', 1, '2025-05-22 00:14:32'),
+(87, 8, 1, 'follower', NULL, 'Te ha seguido.', 1, '2025-05-22 00:14:33'),
 (88, 1, 7, 'follower', NULL, 'Te ha seguido.', 1, '2025-05-22 00:16:29'),
 (89, 3, 1, 'follower', NULL, 'Te ha seguido.', 0, '2025-05-22 00:37:08'),
 (90, 7, 1, 'follower', NULL, 'Te ha seguido.', 0, '2025-05-22 00:38:35'),
 (91, 9, 1, 'follower', NULL, 'Te ha seguido.', 0, '2025-05-22 00:38:36'),
 (92, 3, 1, 'follower', NULL, 'Te ha seguido.', 0, '2025-05-22 00:38:36'),
-(93, 8, 1, 'follower', NULL, 'Te ha seguido.', 0, '2025-05-22 00:38:37'),
+(93, 8, 1, 'follower', NULL, 'Te ha seguido.', 1, '2025-05-22 00:38:37'),
 (94, 7, 1, 'follower', NULL, 'Te ha seguido.', 0, '2025-05-22 00:53:13'),
 (95, 9, 1, 'follower', NULL, 'Te ha seguido.', 0, '2025-05-22 00:53:13'),
 (96, 3, 1, 'follower', NULL, 'Te ha seguido.', 0, '2025-05-22 00:53:13'),
-(97, 8, 1, 'follower', NULL, 'Te ha seguido.', 0, '2025-05-22 00:53:14'),
+(97, 8, 1, 'follower', NULL, 'Te ha seguido.', 1, '2025-05-22 00:53:14'),
 (98, 3, 7, 'follower', NULL, 'Te ha seguido.', 0, '2025-05-22 01:05:55'),
 (99, 9, 7, 'follower', NULL, 'Te ha seguido.', 0, '2025-05-22 01:06:29'),
 (100, 9, 1, 'follower', NULL, 'Te ha seguido.', 0, '2025-05-22 01:15:20'),
@@ -349,7 +339,28 @@ INSERT INTO `notifications` (`id`, `user_id`, `sender_id`, `type`, `reference_id
 (136, 7, 1, 'like', 14, 'Le dio like a tu publicación.', 0, '2025-06-25 16:09:45'),
 (137, 7, 1, 'follower', NULL, 'Te ha seguido.', 0, '2025-06-25 16:10:11'),
 (138, 7, 1, 'like', 15, 'Le dio like a tu publicación.', 0, '2025-06-25 17:36:41'),
-(139, 9, 1, 'follower', NULL, 'Te ha seguido.', 0, '2025-06-25 19:07:01');
+(139, 9, 1, 'follower', NULL, 'Te ha seguido.', 0, '2025-06-25 19:07:01'),
+(140, 7, 1, 'message', NULL, 'Te ha enviado un mensaje.', 0, '2025-06-26 16:30:13'),
+(141, 1, 1, 'like', 26, 'Le dio like a tu publicación.', 1, '2025-06-26 17:00:49'),
+(142, 1, 1, 'like', 26, 'Le dio like a tu publicación.', 1, '2025-06-26 17:00:52'),
+(143, 7, 7, 'like', 33, 'Le dio like a tu publicación.', 0, '2025-06-26 17:32:24'),
+(144, 7, 7, 'like', 35, 'Le dio like a tu publicación.', 0, '2025-06-26 17:33:14'),
+(145, 7, 7, 'like', 34, 'Le dio like a tu publicación.', 0, '2025-06-26 17:33:14'),
+(146, 7, 7, 'like', 36, 'Le dio like a tu publicación.', 0, '2025-06-26 17:33:15'),
+(147, 1, 1, 'comment', 31, 'Comentó tu publicación.', 1, '2025-06-26 18:35:47'),
+(148, 1, 1, 'comment', 32, 'Comentó tu publicación.', 1, '2025-06-26 18:36:08'),
+(149, 8, 1, 'follower', NULL, 'Te ha seguido.', 0, '2025-06-26 18:45:23'),
+(150, 8, 1, 'like', 38, 'Le dio like a tu publicación.', 0, '2025-06-26 18:46:54'),
+(151, 8, 1, 'like', 37, 'Le dio like a tu publicación.', 0, '2025-06-26 18:46:55'),
+(152, 8, 8, 'like', 37, 'Le dio like a tu publicación.', 0, '2025-06-26 18:47:00'),
+(153, 8, 8, 'like', 38, 'Le dio like a tu publicación.', 0, '2025-06-26 18:47:01'),
+(154, 7, 1, 'comment', 39, 'Comentó tu publicación.', 0, '2025-06-26 18:47:43'),
+(155, 7, 1, 'comment', 39, 'Comentó tu publicación.', 0, '2025-06-26 18:48:09'),
+(156, 7, 1, 'follower', NULL, 'Te ha seguido.', 0, '2025-06-26 19:00:33'),
+(157, 8, 1, 'follower', NULL, 'Te ha seguido.', 0, '2025-06-26 19:02:33'),
+(158, 7, 1, 'follower', NULL, 'Te ha seguido.', 0, '2025-06-26 19:05:56'),
+(159, 12, 1, 'follower', NULL, 'Te ha seguido.', 0, '2025-06-26 19:06:03'),
+(160, 7, 1, 'like', 39, 'Le dio like a tu publicación.', 0, '2025-06-26 19:06:07');
 
 -- --------------------------------------------------------
 
@@ -372,7 +383,8 @@ CREATE TABLE `password_resets` (
 
 INSERT INTO `password_resets` (`id`, `user_id`, `tokeen`, `expires_at`, `used`, `created_at`) VALUES
 (1, 2, '$2y$10$83SUseqp2R4pfPZEq5Axp.Uivrz3H2Fz1IbFanETU0Pw2hteigCJu', '2025-06-18 19:25:33', 1, '2025-06-18 11:25:33'),
-(2, 2, '$2y$10$Ze8OLIYfw2AootIm3Kwt6eJukj5YhahSFo9BoDRBeyxT7nPBxWfPu', '2025-06-18 19:36:14', 1, '2025-06-18 11:36:14');
+(2, 2, '$2y$10$Ze8OLIYfw2AootIm3Kwt6eJukj5YhahSFo9BoDRBeyxT7nPBxWfPu', '2025-06-18 19:36:14', 1, '2025-06-18 11:36:14'),
+(3, 1, '$2y$10$3SKqKyt2XrMbUlzlWyTt6.cmttR4ZcIF3QGTcddwIMKn89Y8rpg/6', '2025-06-26 20:26:54', 0, '2025-06-26 12:26:54');
 
 -- --------------------------------------------------------
 
@@ -396,24 +408,19 @@ CREATE TABLE `posts` (
 --
 
 INSERT INTO `posts` (`id`, `user_id`, `image`, `file_name`, `type`, `caption`, `is_blurred`, `created_at`) VALUES
-(12, 1, '', '1745631632_680c3990bbcad', 'jpg', 'drsggsd', 0, '2025-04-26 03:40:32'),
-(13, 1, '', '1745631691_680c39cb52ff8', 'jpg', 'aaa', 0, '2025-04-26 03:41:31'),
-(14, 7, '', '1745636274_680c4bb27529c', 'jpg', 'Folleto 2', 0, '2025-04-26 04:57:54'),
-(15, 7, '', '1745636290_680c4bc2d375c', 'jpg', 'Verbos de objetivossd', 0, '2025-04-26 04:58:10'),
-(16, 1, '', '1745791647_680eaa9faa510', 'jpg', 'abc', 0, '2025-04-28 00:07:27'),
-(17, 1, '', '1745791686_680eaac6711bf', 'jpg', '123', 0, '2025-04-28 00:08:06'),
-(19, 1, '', '1745791744_680eab0049285', 'png', '123', 0, '2025-04-28 00:09:04'),
-(21, 1, '', '1745795724_680eba8c0147f', 'jpg', 'Falda', 0, '2025-04-28 01:15:24'),
 (22, 1, '', '1745849855_680f8dffe3748', 'mp4', 'Mi video de vacaciones en Velero por el mar caribe tomando el sol', 0, '2025-04-28 16:17:35'),
 (24, 1, '', '1746623974_681b5de67edbe', 'jpg', 'mujer', 0, '2025-05-07 15:19:34'),
 (25, 1, '', '1746624008_681b5e0830f24', 'jpg', 'hombre\r\n', 0, '2025-05-07 15:20:08'),
 (26, 1, '', '1746624067_681b5e43383ec', 'jpg', 'mujer cuerpo completo', 0, '2025-05-07 15:21:07'),
-(27, 1, '', '1746627266_681b6ac2cdd1e', 'mp4', 'Video Reel\r\n', 1, '2025-05-07 16:14:26'),
-(28, 1, '', '1749260307_6843981331138', 'jpg', 'Mapa', 0, '2025-06-07 03:38:27'),
-(29, 1, '', '1749431362_68463442cde12', 'png', 'cultura fisica', 0, '2025-06-09 03:09:22'),
-(30, 1, '', '1749432618_6846392a8659b', 'jpeg', 'a', 0, '2025-06-09 03:30:18'),
-(31, 1, '', '1749432679_68463967c3459', 'mp4', 'a', 0, '2025-06-09 03:31:19'),
-(32, 1, '', '1749432823_684639f766248', 'mp4', 'v', 0, '2025-06-09 03:33:43');
+(27, 1, '', '1746627266_681b6ac2cdd1e', 'mp4', 'Video Corto\n', 0, '2025-05-07 16:14:26'),
+(32, 1, '', '1749432823_684639f766248', 'mp4', 'Mi video de casi 10 minutos ', 0, '2025-06-09 03:33:43'),
+(33, 7, '', '1750951936_685d68006ce61', 'jpg', '', 0, '2025-06-26 17:32:16'),
+(34, 7, '', '1750951951_685d680fe52bf', 'jpg', '', 0, '2025-06-26 17:32:31'),
+(35, 7, '', '1750951968_685d68208bd61', 'jpg', '', 0, '2025-06-26 17:32:48'),
+(36, 7, '', '1750951979_685d682b5878d', 'jpg', '', 0, '2025-06-26 17:32:59'),
+(37, 8, '', '1750956276_685d78f471e58', 'jpg', 'Mujer', 0, '2025-06-26 18:44:36'),
+(38, 8, '', '1750956283_685d78fb6b239', 'jpg', 'Imagen de IA', 0, '2025-06-26 18:44:43'),
+(39, 7, '', '1750956299_685d790bcb6f5', 'png', '', 0, '2025-06-26 18:44:59');
 
 -- --------------------------------------------------------
 
@@ -439,14 +446,14 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `fullname`, `username`, `email`, `password`, `profile_photo_type`, `bio`, `birth_date`, `created_at`, `updated_at`) VALUES
-(1, 'Mauricio Muñoz', 'mauoz', 'mauricio@gmail.com', '$2y$10$pnylrutmifOawwvYycp0weFzOVKl9WAVB9XqWlsp2IC92WN/pe1QG', 'jpg', 'Hola\r\nEste es mi perfil de Focuz que utilizo para subir y reproducir videoss', '2025-04-08', '2025-04-15 20:02:01', '2025-04-15 20:02:01'),
-(2, 'Carlos Sanchez', 'carlitos2', 'mauriciomuozsanchez12@gmail.com', '$2y$10$FpV3JHJS.nSaaqt127mUjutT2W7bUj5dFneMXfJTzTcup.1akNLwG', '', 'hi', '2015-04-07', '2025-04-15 23:58:48', '2025-04-15 23:58:48'),
-(3, 'Mauricio 24', '', 'Mauricio@gmail.com', '', '', NULL, '0000-00-00', '0000-00-00 00:00:00', '0000-00-00 00:00:00'),
-(4, 'Mauricio', '', 'Mauricio@gmail.com', '', '', NULL, '0000-00-00', '0000-00-00 00:00:00', '0000-00-00 00:00:00'),
+(1, 'Mauricio Muñoz', 'mauoz', 'mauriciomuozsanchez12@gmail.com', '$2y$10$pnylrutmifOawwvYycp0weFzOVKl9WAVB9XqWlsp2IC92WN/pe1QG', 'jpg', 'Hola\r\nEste es mi perfil de CityScout donde puedo compartir diferentes imagenes y reproducir videos...', '2025-04-08', '2025-04-15 20:02:01', '2025-04-15 20:02:01'),
+(2, 'Carlos Sanchez', 'carlitos2', 'mauro@gmail.com', '$2y$10$FpV3JHJS.nSaaqt127mUjutT2W7bUj5dFneMXfJTzTcup.1akNLwG', '', 'hi', '2015-04-07', '2025-04-15 23:58:48', '2025-04-15 23:58:48'),
+(3, 'Mauricio 24', 'Mauro24', 'Mauricio@gmail.com', '', '', NULL, '0000-00-00', '0000-00-00 00:00:00', '0000-00-00 00:00:00'),
+(4, 'Mauricio', 'Mao520', 'Mauricio1@gmail.com', '', '', NULL, '0000-00-00', '0000-00-00 00:00:00', '0000-00-00 00:00:00'),
 (7, 'Juan Camilo', 'camilocai', 'camilo@mail.com', '$2y$10$sGDUSO35XetkR6UoBKPoW.TVI6U1q69rBuJdLCRWGGQ7xutlv8lwy', 'jpg', 'Hola soy Camilo', '2025-04-01', '2025-04-20 18:12:27', '2025-04-20 18:12:27'),
 (8, 'Andres Lopez', 'Andreslop', 'andres@gmail.com', '$2y$10$DmE9P5oXwV.iLV6tYr6B..Zbsw0rf.7wwrUfR72INuVp7QbrsY3e6', '', NULL, '1999-05-12', '2025-04-27 22:40:06', '2025-04-27 22:40:06'),
 (9, 'Pepito', 'pepito1', 'pepito@mail.com', '$2y$10$k8j.h6KH/Hm1eiXwZYSPk.5oks7OQlQiRo1Ka5DVxfhIoEaNr9CX6', '', NULL, '1221-12-12', '2025-04-27 23:57:56', '2025-04-27 23:57:56'),
-(10, '1', '1', 'mauricio.andres.munoz@correounivalle.edu.co', '$2y$10$eK3P7Ic698K1eMW3MP5bbeYDzyo6aSPMAOBmBQWteLSbZg8JAeAi.', '', NULL, '1111-11-11', '2025-04-28 00:18:12', '2025-04-28 00:18:12'),
+(10, '1', 'sinname', 'mauricio.andres.munoz@correounivalle.edu.co', '$2y$10$eK3P7Ic698K1eMW3MP5bbeYDzyo6aSPMAOBmBQWteLSbZg8JAeAi.', '', NULL, '1111-11-11', '2025-04-28 00:18:12', '2025-04-28 00:18:12'),
 (12, 'Camilo Astudillo', 'camilocaii', 'camilo@gmail.com', '$2y$10$EVJ/zwoHatCKHHBvYlu4IuQGKgnvE1W4zyQgy4L/ugmQlomR/MyB6', '', NULL, '1999-05-12', '2025-05-15 03:10:17', '2025-05-15 03:10:17');
 
 -- --------------------------------------------------------
@@ -479,10 +486,11 @@ INSERT INTO `user_interests` (`id`, `user_id`, `interest_id`) VALUES
 (723, 9, 8),
 (724, 9, 3),
 (737, 9, 5),
-(1197, 1, 4),
-(1198, 1, 6),
-(1199, 1, 5),
-(1200, 1, 8);
+(1431, 1, 8),
+(1432, 1, 7),
+(1433, 1, 9),
+(1434, 1, 6),
+(1435, 1, 5);
 
 --
 -- Índices para tablas volcadas
@@ -570,13 +578,13 @@ ALTER TABLE `user_interests`
 -- AUTO_INCREMENT de la tabla `comments`
 --
 ALTER TABLE `comments`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=29;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=33;
 
 --
 -- AUTO_INCREMENT de la tabla `followers`
 --
 ALTER TABLE `followers`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=61;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=66;
 
 --
 -- AUTO_INCREMENT de la tabla `interests`
@@ -588,31 +596,31 @@ ALTER TABLE `interests`
 -- AUTO_INCREMENT de la tabla `likes`
 --
 ALTER TABLE `likes`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=151;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=162;
 
 --
 -- AUTO_INCREMENT de la tabla `messages`
 --
 ALTER TABLE `messages`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
 
 --
 -- AUTO_INCREMENT de la tabla `notifications`
 --
 ALTER TABLE `notifications`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=140;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=161;
 
 --
 -- AUTO_INCREMENT de la tabla `password_resets`
 --
 ALTER TABLE `password_resets`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT de la tabla `posts`
 --
 ALTER TABLE `posts`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=33;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=40;
 
 --
 -- AUTO_INCREMENT de la tabla `users`
@@ -624,7 +632,7 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT de la tabla `user_interests`
 --
 ALTER TABLE `user_interests`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1201;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1436;
 
 --
 -- Restricciones para tablas volcadas
